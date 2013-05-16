14a53d359a524baf27adac376c1770e783fa565d
Matt Steele <orphum@gmail.com>
2013-05-15 19:27:51 -0500

    Adding scss and wayne's world classes

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+            waynes: {
+                      files: {
+                               'waynesworld.css' : 'waynesworld.scss'
+                             }
+                    }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task.
+  grunt.registerTask('default', ['sass']);
+
+};
