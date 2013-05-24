5563b037273c283e19b7dd783724068750ac606c
Matt Steele <orphum@gmail.com>
2013-05-24 15:11:08 -0500

    Wrap JS code in IIFE

--- js/battery.js
+++ js/battery.js
@@ -1,21 +1,21 @@
-'use strict';
+(function() {
+  $('.percent').text('Battery: ' + (navigator.battery.level * 100) + '%');
 
-$('.percent').text('Battery: ' + (navigator.battery.level * 100) + '%');
-
-var batteryFunction = function(event) {
-  var battery = navigator.battery;
-  $('.isCharging').text('The battery ' + (battery.charging ? 'is' : 'is not') + ' charging!');
-  var $page = $('html');
-  if (battery.charging) {
-    $page.addClass('green');
-    $page.removeClass('red');
-  } else {
-    $page.addClass('red');
-    $page.removeClass('green');
-  }
-};
-$(function() {
-  batteryFunction();
-});
-navigator.battery.addEventListener('levelchange', batteryFunction);
-navigator.battery.addEventListener('chargingchange', batteryFunction);
+  var batteryFunction = function(event) {
+    var battery = navigator.battery;
+    $('.isCharging').text('The battery ' + (battery.charging ? 'is' : 'is not') + ' charging!');
+    var $page = $('html');
+    if (battery.charging) {
+      $page.addClass('green');
+      $page.removeClass('red');
+    } else {
+      $page.addClass('red');
+      $page.removeClass('green');
+    }
+  };
+  $(function() {
+    batteryFunction();
+  });
+  navigator.battery.addEventListener('levelchange', batteryFunction);
+  navigator.battery.addEventListener('chargingchange', batteryFunction);
+})();
--- js/lightsensor.js
+++ js/lightsensor.js
@@ -1,12 +1,15 @@
-window.addEventListener('devicelight', function(event) {
-  $('.lightlevel').text(event.value);
-  if (event.value < 20) {
-    document.querySelector('audio').play();
-    $('html').addClass('partytime');
-    $('.party').addClass('schwing');
-  } else {
-    document.querySelector('audio').pause();
-    $('.party').removeClass('schwing');
-    $('html').removeClass('partytime');
-  }
-});
+(function() {
+  var lightSensorThreshold = 20;
+  window.addEventListener('devicelight', function(event) {
+    $('.lightlevel').text(event.value);
+    if (event.value < lightSensorThreshold) {
+      document.querySelector('audio').play();
+      $('html').addClass('partytime');
+      $('.party').addClass('schwing');
+    } else {
+      document.querySelector('audio').pause();
+      $('.party').removeClass('schwing');
+      $('html').removeClass('partytime');
+    }
+  });
+})();
